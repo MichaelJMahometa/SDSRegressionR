3d005521d18a8cb617d33a35764a129504d40362
Michael J. Mahometa <michaeljmahometa@gmail.com>
2016-10-14 15:41:33 -0500

    new function for adding predicted values to a dataframe containing NAs.

---
+++ R/predictValues.R
@@ -0,0 +1,20 @@
+#' Predict Values
+#'
+#' Basic function to find fitted values for items in an original data frame.
+#'
+#' @param obj Model object from an lm() fitted equation.
+#'
+#' @examples
+#'  mod <- lm(hp ~ disp + wt, data=mtcars)
+#'  mtcars$pred <- predictValues(mod)
+#'
+#'  @export
+predictValues <- function(obj){
+  orig_data_rows <- row.names(get(paste(obj$call$data)))
+  vect <- rep(NA, length(orig_data_rows))
+  names(vect) <- orig_data_rows
+  fit_names <- names(fitted(obj))
+  vect[which(orig_data_rows %in% fit_names)] <- fitted(obj)
+  return(vect)
+}
+
