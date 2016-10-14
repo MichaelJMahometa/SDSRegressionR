107c502cc960de63643797312d3ee2906889cdbf
Michael J. Mahometa <michaeljmahometa@gmail.com>
2016-10-14 12:03:06 -0500

    Fixed to handle interaction models

---
+++ R/lmBeta.R
@@ -0,0 +1,20 @@
+#' Standardized Betas
+#'
+#' Simple function to produce standardized beta estimates after a model run in lm().
+#'
+#' @param obj Model object from an lm() fitted equation.
+#'
+#' @examples
+#' mod <- lm(hp ~ disp + wt, data=mtcars)
+#' summary(mod)
+#' lmBeta(mod)
+#'
+#' @export
+lmBeta <- function(obj){
+b <- summary(obj)$coef[-1, 1]
+sx <- sapply(data.frame(model.matrix(obj)[,-1], stringsAsFactors = FALSE), sd, na.rm=TRUE)
+sy <- sd(as.numeric(unlist(data.frame(model.frame(obj)[,1]))), na.rm=TRUE)
+beta <- b * sx/sy
+names(beta) <- names(data.frame(model.matrix(obj))[-1])
+return(beta)
+}
