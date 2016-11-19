9603e03e1be59cd20d4b2f3133bd2ab76aa57186
Michael J. Mahometa <michaeljmahometa@gmail.com>
2016-11-19 12:47:44 -0600

    Turned OFF

--- R/tagObs_depreciated.R
+++ R/tagObs_depreciated.R
@@ -1,18 +1,18 @@
 
-tagObs <- function(obj, tag=1, name=NULL){
-  thisdf <- get(paste(eval(obj)$call$data))
-  pars <- as.list(match.call()[-1])
-  md <- as.character(pars$obj)
-  n <- names(thisdf)
-  i <- names(obj$fitted.values)
-
-  #thisdf <- data.frame(thisdf[names(fitted.values(model)[i]),], tag) #Possible for use to create "Used obs and variables" dataframe....
-  #thisdf[, length(thisdf)+1] <- 0
-  thisdf[i, length(thisdf)+1] <- tag
-  if(is.null(name)){
-    names(thisdf) <- c(n, paste("In_Model_", md, sep=""))
-  } else {
-    names(thisdf) <- c(n, paste("In_Model_", name, sep=""))
-  }
-  thisdf
-}
+# tagObs <- function(obj, tag=1, name=NULL){
+#   thisdf <- get(paste(eval(obj)$call$data))
+#   pars <- as.list(match.call()[-1])
+#   md <- as.character(pars$obj)
+#   n <- names(thisdf)
+#   i <- names(obj$fitted.values)
+#
+#   #thisdf <- data.frame(thisdf[names(fitted.values(model)[i]),], tag) #Possible for use to create "Used obs and variables" dataframe....
+#   #thisdf[, length(thisdf)+1] <- 0
+#   thisdf[i, length(thisdf)+1] <- tag
+#   if(is.null(name)){
+#     names(thisdf) <- c(n, paste("In_Model_", md, sep=""))
+#   } else {
+#     names(thisdf) <- c(n, paste("In_Model_", name, sep=""))
+#   }
+#   thisdf
+# }
