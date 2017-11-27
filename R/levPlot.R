1731f8c387fe12f5ae129b2671a4548831248dc7
Michael J. Mahometa <michaeljmahometa@gmail.com>
2017-11-27 08:57:12 -0600

    Updates for cut.offs.

--- R/levPlot.R
+++ R/levPlot.R
@@ -4,7 +4,8 @@
 #'
 #' @inheritParams studResidPlot
 #' @param ylim Optional ylim for the plot.
-#' @param save.cutoff Logical: Should the average hat value be saved. The standard cut-offs (graphed) at 2 and 3 times this average hat value.
+#' @param save.cutoff Logical: Should the used cutoff of the hat value be saved. The standard cut-offs (graphed) at 2 and 3 times the average hat value ($(k+1) / n$).
+#' @param cut.level Level to be used in *printing* the list of Leverage outliers. Defaults to 3 times the average hat value ($(k+1) / n$).
 #'
 #' @seealso
 #' \code{\link{studResidPlot}}
@@ -17,18 +18,19 @@
 #' leveragePlot(mod)
 #'
 #' @export
-levPlot <- function(obj, ylim=NULL, key.variable=NULL, print.obs=FALSE, print.plot=TRUE, sort.obs=FALSE, save.cutoff=FALSE){
+levPlot <- function(obj, ylim=NULL, key.variable=NULL, print.obs=FALSE, print.plot=TRUE, sort.obs=FALSE, save.cutoff=FALSE, cut.level=3){
   thisdf <- get(paste(eval(obj)$call$data))
   #thisdf <- obj$model
-  K <- length(coef(obj))
+  P <- length(coef(obj))
   N <- length(hatvalues(obj))
-  hat.avg <- K/N
+  hat.avg <- P/N
   if (print.plot == TRUE){
   plot(hatvalues(obj), ylim=ylim, pch=16, main="Leverage", ylab="Leverage (Hat Values)")
   abline(h=c(2,3)*hat.avg, lty=2, col=c("orange", "red"))
   }
   if(save.cutoff){
-    assign("cooksCutOff", cutoff, envir = .GlobalEnv)
+    hatCutoffs <- list(2*hat.avg, 3*hat.avg)
+    assign("hatCutoffs", hatCutoffs, envir = .GlobalEnv)
   }
   if(print.obs) {
     if (any(class(thisdf) == "tbl_df") & is.null(key.variable)){
@@ -37,7 +39,7 @@ levPlot <- function(obj, ylim=NULL, key.variable=NULL, print.obs=FALSE, print.pl
     } else if (!is.null(key.variable)){
       #if tibble & NULL key.variable != NULL (key.variable="Subject_ID")
       thisdf <- add_column(thisdf, rn = row.names(thisdf), .before=key.variable)
-      i <- names(hatvalues(obj))[hatvalues(obj) > hat.avg * 2]
+      i <- names(hatvalues(obj))[hatvalues(obj) > hat.avg * cut.level]
       n <- names(obj$model)
       rep_df <- data.frame(thisdf[which(thisdf$rn %in% i), key.variable],
                            thisdf[which(thisdf$rn %in% i), n],
@@ -46,7 +48,7 @@ levPlot <- function(obj, ylim=NULL, key.variable=NULL, print.obs=FALSE, print.pl
       names(rep_df) <- c(key.variable, n, "Predicted_Y", "Hat_Values")
     } else {
       #if data.frame (or maybe data.table?)
-      i <- names(hatvalues(obj))[hatvalues(obj) > hat.avg * 2]
+      i <- names(hatvalues(obj))[hatvalues(obj) > hat.avg * cut.level]
       n <- names(obj$model)
       rep_df <- data.frame(i,
                            thisdf[which(row.names(thisdf) %in% i), n],
