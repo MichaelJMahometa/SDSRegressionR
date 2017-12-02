1514024d68d7dcd16cd42a340fb1755a03501196
Michael J. Mahometa <michaeljmahometa@gmail.com>
2017-12-02 15:40:17 -0600

    Changes to truncate.

--- R/dfBetas.R
+++ R/dfBetas.R
@@ -1,11 +1,12 @@
 #' Return DFBetas or DFBeta values
 #'
-#' Simple function to produce a listing of all DFBetas (or DFBeta) values for the coefficients in a lm() object. Observations to investigate (tagged as Investigate) are row sums of absolute coefficients greater than 2 / sqrt(N).
+#' Simple function to produce a listing of all DFBetas (or DFBeta) values for the coefficients in a lm() object. Observations to investigate (tagged as Investigate) are row sums of absolute coefficients greater than 2 / sqrt(N). Only observations with a Investigte value of 1 or greater are printed by default.
 #'
 #' @param obj Object from an lm() fiitted equation.
-#' @param key.variable Used if lm() data object is of tibble class. Name of the unique key variable (identifier variable). If data object is of data.frame class, row.names will be used instead.
-#' @param sort Logical: Should the returned dataframe be sorted by the number of tagged coefficients?
+#' @param key.variable Required if lm() data object is of tibble class. Name of the unique key variable (identifier variable). If data object is of data.frame class, this is optional and row.names will be used instead.
+#' @param sort.obs Logical: Should the returned dataframe be sorted by the number of tagged coefficients?
 #' @param standardize Logicial: Should standardized DFBeta values (DFBetas) be returned in the dataframe?
+#' @param truncate Logical: Should the output be truncated to show ONLY observations that meet the cut-off standardized difference betas of \eqn{2/sqrt(N)} (the default) or ALL observations in the model?
 #'
 #' @seealso
 #' \code{\link{studResidPlot}}
@@ -20,13 +21,13 @@
 #' dfBetas(mod)
 #'
 #' @export
-dfBetas <- function(obj, key.variable = NULL, sort=FALSE, standardize=TRUE){
+dfBetas <- function(obj, key.variable = NULL, sort.obs=FALSE, standardize=TRUE, truncate=TRUE){
   thisdf <- get(paste(eval(obj)$call$data))
   tbetas <- data.frame(dfbetas(obj))
   tbeta <- data.frame(dfbeta(obj))
   if (any(class(thisdf) == "tbl_df") & is.null(key.variable)) {
     stop("Data is of tibble class -- key.variable must be supplied.")
-  } else if (any(class(thisdf) == "tbl_df") & !is.null(key.variable)) {
+  } else if (!is.null(key.variable)) {
     thisdf <- add_column(thisdf, rn = row.names(thisdf), .before = key.variable)
     i <- row.names(tbetas)
     N <- nrow(thisdf)
@@ -64,8 +65,16 @@ dfBetas <- function(obj, key.variable = NULL, sort=FALSE, standardize=TRUE){
       names(mtdf) <- c("row.names", n, "Investigate")
     }
   }
-  if(sort){
+  if(sort.obs){
     mtdf <- mtdf[order(-mtdf$Investigate), ]
+  }
+  if(truncate){
+  num_invest <- sum(mtdf$Investigate)
+  if(num_invest == 0){
+    stop("No observations reach cut-off.")
+  } else {
+    mtdf <- mtdf[which(mtdf$Investigate >= 1),]
+  }
   }
   return(mtdf)
 }
